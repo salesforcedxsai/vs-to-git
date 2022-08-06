aee4d5aa6fa76eebfcf2ccb45bbf6ad303ede1ec
salesforcedxsai <salesforcedxsai@gmail.com>
2022-08-06 19:58:17 +0530

    Nita to Aswath

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'Nita Bhargavi Dangeti';
+  greeting = 'Ashwath';
   changeHandler(event) {
     this.greeting = event.target.value;
   }
