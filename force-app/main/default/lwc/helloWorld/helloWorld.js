ddc7e9c2a895f6151f6dea280656257e425d9a42
salesforcedxsai <salesforcedxsai@gmail.com>
2022-08-06 19:51:38 +0530

    Aadded world yo Nita

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'World';
+  greeting = 'Nita Bhargavi Dangeti';
   changeHandler(event) {
     this.greeting = event.target.value;
   }
